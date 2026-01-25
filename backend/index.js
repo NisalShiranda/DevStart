c06aba4fc64c9431f0c96841bdc04359d1a400ea
Nisal Shiranda <nisalshiranda001@gmail.com>
2026-01-25 20:35:55 +0530

    Folder structuring and dependency downloading

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+require('dotenv').config(); // Load variables from .env file
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
