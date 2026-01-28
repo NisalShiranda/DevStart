825d54d807c9dbdc472d491fd6a842ba92e9b033
Nisal Shiranda <nisalshiranda001@gmail.com>
2026-01-28 20:34:15 +0530

    Started to add admin and user feactures and bookmark for projects

--- backend/models/User.js
+++ backend/models/User.js
@@ -13,7 +13,17 @@ const UserSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true
-  }
+  },// --- අලුත් කොටස 1: ROLE (Admin ද User ද?) ---
+  role: {
+    type: String,
+    default: 'user', // කවුරු රෙජිස්ටර් වුනත් මුලින්ම එයා සාමාන්‍ය 'user' කෙනෙක්
+    enum: ['user', 'admin'] 
+  },
+  // --- අලුත් කොටස 2: SAVED PROJECTS (Bookmark කරපුවා) ---
+  savedProjects: [{
+    type: mongoose.Schema.Types.ObjectId, // අපි මෙතන සේව් කරන්නේ Project එකේ ID එක විතරයි
+    ref: 'Project' // ඒ ID එක අයිති 'Project' කියන මොඩල් එකට
+  }]
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
